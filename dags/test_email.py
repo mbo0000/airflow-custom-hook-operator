78bb00cc7f3ea8daaa7afaec49a6215b1ec162a5
T N <tn@MacBook-Pro-cua-T.local>
2024-07-25 15:31:16 -0700

    config smtp mail

--- dags/test_email.py
+++ dags/test_email.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.utils.email import send_email
+from airflow.hooks.base_hook import BaseHook
 
 default_args = {
     'retries': 1
@@ -12,20 +13,26 @@ default_args = {
     , 'email_on_retry' : True
 }
 
-def send_my_email():
-    send_email('mbo0000da@gmail.com', 'test', 'test', None)    
+# def send_my_email():
+#     conns = BaseHook.get_connection('smtp_default') 
+#     print(conns.host)
+#     print(conns.login)
+#     print(conns.port)
+#     print(conns)
+#     send_email(to = 'mbo0000da@gmail.com', subject = 'test', html_content = 'test')
 
 with DAG(
     dag_id = 'test_email'
     ,start_date = datetime(2024,1,1), catchup=False, schedule = '@daily', default_args = default_args
 ) as dag:
-    # task1 = BashOperator(
+    task1 = BashOperator(
+        task_id = 'task1'
+        , bash_command = 'cd not_real_dir'
+    )
+    task1
+
+    # task1 = PythonOperator(
     #     task_id = 'task1'
-    #     , bash_command = 'cd not_real_dir'
+    #     , python_callable = send_my_email
     # )
     # task1
-
-    task1 = PythonOperator(
-        task_id = 'task1'
-        , python_callable = send_my_email
-    )
