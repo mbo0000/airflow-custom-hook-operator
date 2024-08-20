c8b947f22b1cb9a299d073611ed98800b41e4547
Minh <minh.bo@checkr.com>
2024-08-20 09:50:37 -0700

    remove multi client calls

--- plugins/s3_exporter/s3_hook.py
+++ plugins/s3_exporter/s3_hook.py
@@ -14,21 +14,21 @@ class S3Hook(BaseHook):
         if not self.access_key or not self.secret:
             logging.error('Missing AWS Credentials')
 
-    def upload(self, local_file, s3_file_key):
-
-        if not self._bucket_exist(self.bucket_name):
-            return None
-
-        client = boto3.client(
+        self.client = boto3.client(
             's3',
             region_name             = 'us-west-1',
             aws_access_key_id       = self.access_key,
             aws_secret_access_key   = self.secret
         )
 
+    def upload(self, local_file, s3_file_key):
+
+        if not self._bucket_exist(self.bucket_name):
+            return None
+
         try:
             logging.info(f'Uploading file to {self.bucket_name} is starting.')
-            client.upload_file(
+            self.client.upload_file(
                 Filename    = local_file
                 , Bucket    = self.bucket_name
                 , Key       = s3_file_key
@@ -41,16 +41,9 @@ class S3Hook(BaseHook):
         if not bucket:
             bucket = self.bucket_name
             logging.info(f'Using default bucket: {bucket}')
-        
-        client = boto3.client(
-            's3',
-            region_name             = 'us-west-1',
-            aws_access_key_id       = self.access_key,
-            aws_secret_access_key   = self.secret
-        )
 
         logging.info(f'Checking for bucket: {bucket}')
-        res     = client.list_buckets()['Buckets']
+        res     = self.client.list_buckets()['Buckets']
         buckets = [b['Name'] for b in res]
         if bucket in buckets:
             logging.info(f"Bucket {bucket} found.")
